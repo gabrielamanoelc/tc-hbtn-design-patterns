93839e08a3dd0232a87d024edf673a79cff80a8b
gabrielamanoelc <104454909+gabrielamanoelc@users.noreply.github.com>
2022-07-07 17:11:02 -0300

    Update Builder.java

--- 0x00/personagem-builder/Builder.java
+++ 0x00/personagem-builder/Builder.java
@@ -1,4 +1,11 @@
-public interface Builder { public void setNome(String nome); public void setTipo(TipoPersonagem tipo); public void setForca(int forca); 
-    public void setInteligencia(int inteligencia); public void setVigor(int vigor); public void setResistencia(int resistencia); public 
-    void setDestreza(int destreza);
+public interface Builder {
+
+    public void setNome(String nome);
+
+    public void setTipo(TipoPersonagem tipo);
+    public void setForca(int forca);
+    public void setInteligencia(int inteligencia);
+    public void setVigor(int vigor);
+    public void setResistencia(int resistencia);
+    public void setDestreza(int destreza);
 }
