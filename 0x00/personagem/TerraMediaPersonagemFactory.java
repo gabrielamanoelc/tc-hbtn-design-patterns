3a57a734bd8f1ca2acb4a0baea7beeb6e1b048fe
gabrielamanoelc <104454909+gabrielamanoelc@users.noreply.github.com>
2022-07-07 17:08:50 -0300

    Update TerraMediaPersonagemFactory.java

--- 0x00/personagem/TerraMediaPersonagemFactory.java
+++ 0x00/personagem/TerraMediaPersonagemFactory.java
@@ -1,6 +1,9 @@
-public class TerraMediaPersonagemFactory extends PersonagemFactory{ @Override public Personagem createPersonagem(TipoPersonagem tipo, 
-    String nome) {
-        if(tipo == TipoPersonagem.MAGO){ return new Mago(nome, 10, 2, 5, 3,4);
+public class TerraMediaPersonagemFactory extends PersonagemFactory{
+
+    @Override
+    public Personagem createPersonagem(TipoPersonagem tipo, String nome) {
+        if(tipo == TipoPersonagem.MAGO){
+            return new Mago(nome, 10, 2, 5, 3,4);
         } else if(tipo == TipoPersonagem.LADRAO){
             return new Ladrao(nome, 2, 6, 8, 5, 10);
         } else if(tipo == TipoPersonagem.GUERREIRO){
