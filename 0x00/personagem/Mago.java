15648082b3be753ac97b8d662dd274add6aaa8cb
gabrielamanoelc <104454909+gabrielamanoelc@users.noreply.github.com>
2022-07-07 17:07:10 -0300

    Update Mago.java

--- 0x00/personagem/Mago.java
+++ 0x00/personagem/Mago.java
@@ -1,10 +1,19 @@
-import java.text.DecimalFormat; public class Mago extends Personagem{ public Mago(String nome, int inteligencia, int forca, int vigor, 
-    int resistencia, int destreza) {
-        super(nome, TipoPersonagem.MAGO, inteligencia, forca, vigor, resistencia, destreza); if(inteligencia < forca || inteligencia < 
-        destreza){
+import java.text.DecimalFormat;
+
+public class Mago extends Personagem{
+
+    public Mago(String nome, int inteligencia, int forca, int vigor, int resistencia, int destreza) {
+        super(nome, TipoPersonagem.MAGO, inteligencia, forca, vigor, resistencia, destreza);
+        if(inteligencia < forca || inteligencia < destreza){
             throw new RuntimeException("Atributos invalidos para MAGO");
         }
     }
-    @Override public double getDanoAtaque() { return (inteligencia * 0.8) + (forca * 0.05) + (destreza * 0.05) + (vigor * 0.1);
+    @Override
+    public double getDanoAtaque() {
+        return (inteligencia * 0.8) + (forca * 0.05) + (destreza * 0.05) + (vigor * 0.1);
     }
+
+
+
+
 }
