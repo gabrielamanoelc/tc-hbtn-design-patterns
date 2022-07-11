ddf85bbac8465b8a856e0a62e81491ca8f73579c
gabrielamanoelc <104454909+gabrielamanoelc@users.noreply.github.com>
2022-07-10 21:28:53 -0300

    Update CalculadorSalarioService.java

--- 0x01/salario/CalculadorSalarioService.java
+++ 0x01/salario/CalculadorSalarioService.java
@@ -1,12 +1,24 @@
-import java.util.UUID; public class CalculadorSalarioService { public UUID uuid = UUID.randomUUID(); private static 
-    CalculadorSalarioService instancia; private CalculadorSalarioService() {
+import java.util.UUID;
+
+public class CalculadorSalarioService {
+
+    public UUID uuid = UUID.randomUUID();
+    private static CalculadorSalarioService instancia;
+
+    private CalculadorSalarioService() {
+
     }
-    public double calcularSalarioLiquido(double salarioBruto, double valorDescontos, double valorVendas, double percentualComissao){ 
+
+    public double calcularSalarioLiquido(double salarioBruto, double valorDescontos, double valorVendas, double percentualComissao){
         return salarioBruto - valorDescontos + (valorVendas * (percentualComissao / 100));
     }
-    public static CalculadorSalarioService getCalculadorSalarioService(){ if(instancia == null ){ instancia = new 
-            CalculadorSalarioService();
+
+    public static CalculadorSalarioService getCalculadorSalarioService(){
+        if(instancia == null ){
+            instancia = new CalculadorSalarioService();
         }
         return instancia;
     }
+
+
 }
